778235517986c4ffaf245cb8a518b426e3d605ce
Apostolos-Daniel <tolisapostolidis@gmail.com>
2019-06-15 00:37:25 +0100

    add data and razor component

---
+++ Blazor.ServerSide/Data/MovieData/Actor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.ServerSide.Data.MovieData
+{
+    public class Actor
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+        public string Born { get; internal set; }
+        public string PosterImage { get; internal set; }
+    }
+}
---
+++ Blazor.ServerSide/Data/MovieData/ActorService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.ServerSide.Data.MovieData
+{
+    public class ActorService
+    {
+        private IEnumerable<Actor> Actors;
+        private string[] _actors;
+
+        public ActorService()
+        {
+            _actors = File.ReadAllLines(@".\bin\Debug\netcoreapp3.0\Data\MovieData\person_node.csv");
+            Actors = _actors.Skip(1).Select(x =>
+            {
+                var actorAttributes = x.Split(';');
+                return new Actor
+                {
+                    //id:ID(Person);name;born:int;poster_image
+                    Id = Convert.ToInt32(actorAttributes[0]) - 1,
+                    Name = actorAttributes[1],
+                    Born = actorAttributes[2],
+                    PosterImage = actorAttributes[3]
+                };
+            });
+        }
+          
+        public Task<Actor[]> GetActorsAsync(int maxActors)
+        {
+            return Task.FromResult(Actors.Take(maxActors).ToArray());
+        }
+    }
+}
